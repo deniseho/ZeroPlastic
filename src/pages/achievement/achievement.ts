5279e87852548ba96adbb25b3bbda944aca61137
Denise Ho <deniseho630@gmail.com>
2018-08-14 17:25:39 +0100

    modify the way to display badges in achievement page

--- src/pages/achievement/achievement.ts
+++ src/pages/achievement/achievement.ts
@@ -1,42 +1,47 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
 import {AuthServiceProvider} from '../../providers/auth-service/auth-service';
-import { badges } from '../quiz/badges';
-import { User } from '../../providers/auth-service/User';
+import {badges} from '../quiz/badges';
+import {User} from '../../providers/auth-service/User';
 
-@Component({
-  selector: 'page-achievement',
-  templateUrl: 'achievement.html',
-})
+@Component({selector: 'page-achievement', templateUrl: 'achievement.html'})
 
 export class AchievementPage {
-  currentUser: User;
+  currentUser : User;
 
-  badge1: boolean;
-  badge2: boolean;
-  badge3: boolean;
-  badge4: boolean;
-  badge5: boolean;
+  badge1 : boolean;
+  badge2 : boolean;
+  badge3 : boolean;
+  badge4 : boolean;
+  badge5 : boolean;
 
-  badgeList: any[];
+  badgeRecord : number[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private auth : AuthServiceProvider) {
+  badgeList : any[];
+
+  constructor(public navCtrl : NavController, public navParams : NavParams, private auth : AuthServiceProvider) {
     this.currentUser = this
-    .auth
-    .getCurrentUser();
-    
+      .auth
+      .getCurrentUser();
+
     this.badgeList = badges;
-
-    this.badge1 = false;
-    this.badge2 = false;
-    this.badge3 = false;
-    this.badge4 = false;
-    this.badge5 = false;
-
+    this.badgeRecord = [0, 0, 0, 0, 0];
   }
 
   ionViewDidLoad() {
-    console.log(this.currentUser);
+    let score = this.currentUser.totalScore;
+
+    if (score < 25) {
+      this.badgeRecord = [1, 0, 0, 0, 0];
+    } else if (score > 25 && score < 60) {
+      this.badgeRecord = [1, 1, 0, 0, 0];
+    } else if (score > 61 && score < 100) {
+      this.badgeRecord = [1, 1, 1, 0, 0];
+    } else if (score > 100 && score < 150) {
+      this.badgeRecord = [1, 1, 1, 1, 0];
+    } else if (score > 150) {
+      this.badgeRecord = [1, 1, 1, 1, 1];
+    }
   }
 
 }
