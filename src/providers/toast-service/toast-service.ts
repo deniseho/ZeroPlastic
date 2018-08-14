4ddaec02a25c1c605aa80894b4b8b8e71c9d417c
Denise Ho <deniseho630@gmail.com>
2018-08-14 10:36:50 +0100

    add toast service provider

---
+++ src/providers/toast-service/toast-service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ToastController } from 'ionic-angular';
+
+/*
+  Generated class for the ToastServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ToastServiceProvider {
+
+  constructor(private toastCtrl : ToastController) {
+    console.log('Hello ToastServiceProvider Provider');
+  }
+
+  showToast(message : string) {
+    let toast = this
+      .toastCtrl
+      .create({message: message, duration: 2000});
+
+    toast.present(toast);
+  }
+
+}
