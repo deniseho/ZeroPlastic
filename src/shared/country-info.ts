9b90def6980510a2c5aa758210beec27e96f048d
Denise Ho <deniseho630@gmail.com>
2018-08-17 11:06:36 +0100

    add.country name in the country info title

--- src/shared/country-info.ts
+++ src/shared/country-info.ts
@@ -1,16 +1,16 @@
 export const country: any = {
     title: "Top Ten Producers of Plastic Waste in the world",
     countryList: {
-        "China": "<h1>NO. 1</h1> China is responsible for nearly 30% of the plastic waste in our world’s oceans",
-        "Indonesia": "<h1>NO. 2</h1> Indonesia contributes roughly 200,000 tonnes of waste to our world’s oceans",
-        "Philippines": "<h1>NO. 3</h1> The passig river in the Philippines dumps the equivalent of 10,600 elephants of plastic into the pacific ocean every year",
-        "Vietnam": "<h1>NO. 4</h1> 18,000 tonnes of plastic waste are generated in Vietnam on a daily basis",
-        "Sri Lanka": "<h1>NO. 5</h1> While Sri Lanka implemented a ban on single-use plastics in January 2018, the country comes fifth in the world for plastic polluters",
-        "Egypt": "<h1>NO. 6</h1> Egypt produces 16 million tonnes of rubbish per year, with 55% of its plastics recycled",
-        "Thailand": "<h1>NO. 7</h1> In Thailand almost 500 marine life forms die each year, mainly caused by man-made marine debris",
-        "Malaysia": "<h1>NO. 8</h1> Malaysia is the 8th worst country worldwide for plastic waste, Only 15% of Malaysian people recycle",
-        "Nigeria": "<h1>NO. 9</h1> Nigeria’s largest city Lagos generates approximately 600,000 metric tonnes of plastic waste per year, of which 100,000 tonnes finds its way into the ocean",
-        "Bangladesh": "<h1>NO. 10</h1> The use of plastics in Bangladesh has grown to 1200,000 metric tonnes in 2018,compared to just 15,000 metric tonnes in 1990",
+        "China": "<h1>NO.1 China</h1> China is responsible for nearly 30% of the plastic waste in our world’s oceans",
+        "Indonesia": "<h1>NO.2 Indonesia</h1> Indonesia contributes roughly 200,000 tonnes of waste to our world’s oceans",
+        "Philippines": "<h1>NO.3 Philippines</h1> The passig river in the Philippines dumps the equivalent of 10,600 elephants of plastic into the pacific ocean every year",
+        "Vietnam": "<h1>NO.4 Vietnam</h1> 18,000 tonnes of plastic waste are generated in Vietnam on a daily basis",
+        "Sri Lanka": "<h1>NO.5 Sri Lanka</h1> While Sri Lanka implemented a ban on single-use plastics in January 2018, the country comes fifth in the world for plastic polluters",
+        "Egypt": "<h1>NO.6 Egypt</h1> Egypt produces 16 million tonnes of rubbish per year, with 55% of its plastics recycled",
+        "Thailand": "<h1>NO.7 Thailand</h1> In Thailand almost 500 marine life forms die each year, mainly caused by man-made marine debris",
+        "Malaysia": "<h1>NO.8 Malaysia</h1> Malaysia is the 8th worst country worldwide for plastic waste, Only 15% of Malaysian people recycle",
+        "Nigeria": "<h1>NO.9 Nigeria’s</h1> Nigeria’s largest city Lagos generates approximately 600,000 metric tonnes of plastic waste per year, of which 100,000 tonnes finds its way into the ocean",
+        "Bangladesh": "<h1>NO.10 Bangladesh</h1> The use of plastics in Bangladesh has grown to 1200,000 metric tonnes in 2018,compared to just 15,000 metric tonnes in 1990",
     }
 }
 
