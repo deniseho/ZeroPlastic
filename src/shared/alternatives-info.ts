8d6ed2d99e1c97581674b5b8ded21fd5f0240c8b
Denise Ho <deniseho630@gmail.com>
2018-08-22 21:10:47 +0100

    add image content for alternatives

--- src/shared/alternatives-info.ts
+++ src/shared/alternatives-info.ts
@@ -2,40 +2,85 @@ export const alternativesList : any = [
     {
         item: "plasticBags",
         plasticImage: "assets/imgs/Bags/bag.png",
-        plasticText: "160,000 plastic bags are used globally every second, 5 Trillion plastic bags are produced yearly. All together, they circle around the world 7 times! There are much better and more environmentally friendly options available.",
-        alternatives: ["assets/imgs/Bags/bag2.png", "assets/imgs/Bags/bag1.png"],
-        alternativesText1: "Canvas bags: The best replacement for plastic bags are reusable cotton canvas bags. Cheap to buy and very durable, they can be washed and reused as much as you like.",
-        alternativesText2: "Backpack: Another good option, especially with heavy items. It also allows you to keep your hands free in the process."
+        plasticText: "160,000 plastic bags are used globally every second, 5 Trillion plastic bags are" +
+                " produced yearly. All together, they circle around the world 7 times! There are " +
+                "much better and more environmentally friendly options available.",
+        alternatives: [
+            {
+                image: "assets/imgs/Bags/bag2.png",
+                text: "Canvas bags: The best replacement for plastic bags are reusable cotton canvas ba" +
+                        "gs. Cheap to buy and very durable, they can be washed and reused as much as you " +
+                        "like."
+            }, {
+                image: "assets/imgs/Bags/bag1.png",
+                text: "Backpack: Another good option, especially with heavy items. It also allows you t" +
+                        "o keep your hands free in the process."
+            }
+        ]
     }, {
         item: "waterBottles",
         plasticImage: "assets/imgs/Bottles/bottle.png",
         plasticText: "assets/imgs/bunny.png",
-        alternatives: ["assets/imgs/Bottles/bottle1.png", "assets/imgs/Bottles/bottle2.png"],
-        alternativesText: "2"
+        alternatives: [
+            {
+                image: "assets/imgs/Bottles/bottle1.png",
+                text: ""
+            }, {
+                image: "assets/imgs/Bottles/bottle2.png",
+                text: ""
+            }
+        ],
     }, {
         item: "straws",
         plasticImage: "assets/imgs/Straws/straw.png",
         plasticText: "assets/imgs/bunny.png",
         alternatives: [
-            "assets/imgs/Straws/straw1.png",
-            "assets/imgs/Straws/straw2.jpg",
-            "assets/imgs/Straws/straw3.png",
-            "assets/imgs/Straws/straw4.png",
-            "assets/imgs/Straws/straw5.png",
-            "assets/imgs/Straws/straw6.png"
+            {
+                image: "assets/imgs/Straws/straw1.png",
+                text: ""
+            }, {
+                image: "assets/imgs/Straws/straw2.jpg",
+                text: ""
+            },
+            {
+                image: "assets/imgs/Straws/straw3.png",
+                text: ""
+            }, {
+                image: "assets/imgs/Straws/straw4.jpg",
+                text: ""
+            },
+            {
+                image: "assets/imgs/Straws/straw5.png",
+                text: ""
+            }, {
+                image: "assets/imgs/Straws/straw6.jpg",
+                text: ""
+            }
         ],
         alternativesText: "3"
     }, {
         item: "utensils",
         plasticImage: "assets/imgs/Utensils/utensils.png",
         plasticText: "assets/imgs/bunny.png",
-        alternatives: ["assets/imgs/Utensils/utensils1.png", "assets/imgs/Utensils/utensils2.png"],
+        alternatives: [
+            {
+                image: "assets/imgs/Utensils/utensils1.png",
+                text: ""
+            }, {
+                image: "assets/imgs/Utensils/utensils2.png",
+                text: ""
+            }
+        ],
         alternativesText: "4"
     }, {
         item: "toothbrush",
         plasticImage: "assets/imgs/Toothbrush/toothbrush.jpg",
         plasticText: "assets/imgs/bunny.png",
-        alternatives: ["assets/imgs/Toothbrush/toothbrush1.png"],
+        alternatives: [
+            {
+                image: "assets/imgs/Toothbrush/toothbrush1.png",
+                text: ""
+            }],
         alternativesText: "5"
     }
 ]
