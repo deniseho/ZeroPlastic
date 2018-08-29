0c6b631d6f8ff5b26fe9281e5653a19833066304
Denise Ho <deniseho630@gmail.com>
2018-08-29 04:32:57 +0100

    change game items

--- src/pages/game/items.ts
+++ src/pages/game/items.ts
@@ -1,121 +1,120 @@
+export var items : any[] = [
+    {
+        url: 'assets/imgs/game/tag1-1.png',
+        tag: "tag1",
+        recycable: true,
+        speed: 2.5,
+    }, {
+        url: 'assets/imgs/game/tag1-2.png',
+        tag: "tag1",
+        recycable: true,
+        speed: 3,
+    }, 
+    {
+        url: 'assets/imgs/bottle.png',
+        tag: "tag1",
+        recycable: true,
+        speed: 2,
+    }, {
+        url: 'assets/imgs/game/tag2-1.png',
+        tag: "tag2",
+        recycable: true,
+        speed: 1.8,
+    },
+    {
+        url: 'assets/imgs/game/tag2-2.png',
+        tag: "tag2",
+        recycable: true,
+        speed: 3.5,
+    },
+    {
+        url: 'assets/imgs/game/tag3-1.png',
+        tag: "tag3",
+        recycable: false,
+        speed: 2.5,
+    }, {
+        url: 'assets/imgs/game/tag3-2.png',
+        tag: "tag3",
+        recycable: false,
+        speed: 3,
+    }, {
+        url: 'assets/imgs/game/tag3-3.png',
+        tag: "tag3",
+        recycable: false,
+        speed: 1.8,
+    },
+    {
+        url: 'assets/imgs/game/tag4-1.png',
+        tag: "tag4",
+        recycable: true,
+        speed: 2.2,
+    },
+    {
+        url: 'assets/imgs/game/tag4-2.png',
+        tag: "tag4",
+        recycable: true,
+        speed: 0.9,
+    },
+    {
+        url: 'assets/imgs/game/tag5-1.png',
+        tag: "tag5",
+        recycable: true,
+        speed: 2.8,
+    },
+    {
+        url: 'assets/imgs/game/tag5-2.png',
+        tag: "tag5",
+        recycable: false,
+        speed: 1.3,
+    },
+    {
+        url: 'assets/imgs/game/tag6-1.png',
+        tag: "tag6",
+        recycable: false,
+        speed: 1.5,
+    },
+    {
+        url: 'assets/imgs/game/tag6-2.png',
+        tag: "tag6",
+        recycable: false,
+        speed: 2,
+    },
+    {
+        url: 'assets/imgs/fork_knife.png',
+        tag: "tag6",        
+        recycable: false,
+        speed: 3,
+    }
+]
+
+
 // var items : any[] = [
 //     {
-//         url: 'assets/imgs/game/tag1-1.png',
-//         tag: "tag1",
+//         url: 'assets/imgs/cup.png',
+//         name: "Plastic Cup",
+//         type: "tag1",
 //         recycable: true,
 //         speed: 2.5,
 //     }, {
-//         url: 'assets/imgs/game/tag1-2.png',
-//         tag: "tag1",
-//         recycable: true,
-//         speed: 3,
-//     }, 
-//     {
 //         url: 'assets/imgs/bottle.png',
-//         tag: "tag1",
+//         name: "Plastic Cup",
+//         type: "tag2",
 //         recycable: true,
-//         speed: 2,
-//     }, {
-//         url: 'assets/imgs/game/tag2-1.png',
-//         tag: "tag2",
-//         recycable: true,
-//         speed: 1.8,
-//     },
-//     {
-//         url: 'assets/imgs/game/tag2-2.png',
-//         tag: "tag2",
-//         recycable: true,
-//         speed: 3.5,
-//     },
-//     {
-//         url: 'assets/imgs/game/tag3-1.png',
-//         tag: "tag3",
-//         recycable: false,
-//         speed: 2.5,
-//     }, {
-//         url: 'assets/imgs/game/tag3-2.png',
-//         tag: "tag3",
-//         recycable: false,
 //         speed: 3,
 //     }, {
-//         url: 'assets/imgs/game/tag3-3.png',
-//         tag: "tag3",
-//         recycable: false,
+//         url: 'assets/imgs/microplastic.png',
+//         name: "Microplastic",
+//         type: "tag3",
+//         recycable: true,
 //         speed: 1.8,
 //     },
-//     {
-//         url: 'assets/imgs/game/tag4-1.png',
-//         tag: "tag4",
-//         recycable: true,
-//         speed: 3.5,
-//     },
-//     {
-//         url: 'assets/imgs/game/tag4-2.png',
-//         tag: "tag4",
-//         recycable: true,
-//         speed: 3.5,
-//     },
-//     {
-//         url: 'assets/imgs/game/tag5-1.png',
-//         tag: "tag5",
-//         recycable: true,
-//         speed: 3.5,
-//     },
-//     {
-//         url: 'assets/imgs/game/tag5-2.png',
-//         tag: "tag5",
-//         recycable: false,
-//         speed: 3.5,
-//     },
-//     {
-//         url: 'assets/imgs/game/tag6-1.png',
-//         tag: "tag6",
-//         recycable: false,
-//         speed: 3.5,
-//     },
-//     {
-//         url: 'assets/imgs/game/tag6-2.png',
-//         tag: "tag6",
-//         recycable: false,
-//         speed: 3.5,
-//     },
-//     {
+//     , {
 //         url: 'assets/imgs/fork_knife.png',
-//         tag: "tag6",        
+//         name: 'Fork and knife',
+//         type: "tag4",
 //         recycable: false,
 //         speed: 3.5,
 //     }
 // ]
 
 // export {items};
-
-var items : any[] = [
-    {
-        url: 'assets/imgs/cup.png',
-        name: "Plastic Cup",
-        type: "tag1",
-        recycable: true,
-        speed: 2.5,
-    }, {
-        url: 'assets/imgs/bottle.png',
-        name: "Plastic Cup",
-        type: "tag2",
-        recycable: true,
-        speed: 3,
-    }, {
-        url: 'assets/imgs/microplastic.png',
-        name: "Microplastic",
-        type: "tag3",
-        recycable: true,
-        speed: 1.8,
-    },
-    , {
-        url: 'assets/imgs/fork_knife.png',
-        name: 'Fork and knife',
-        type: "tag4",
-        recycable: false,
-        speed: 3.5,
-    }
-]
-
-export {items};
