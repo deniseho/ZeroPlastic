86aefa23385692cfccb14512fda91e1081bd0c47
Denise Ho <deniseho630@gmail.com>
2018-08-29 15:34:17 +0100

    added activity page

--- src/pages/topic-five/topic-five.ts
+++ src/pages/topic-five/topic-five.ts
@@ -1,23 +1,56 @@
 import {Component, ViewChild} from '@angular/core';
-import {IonicPage, NavController, Slides, Content} from 'ionic-angular';
-import { User } from '../../shared/user-model';
-import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import {
+  IonicPage,
+  NavController,
+  Slides,
+  Content,
+  NavParams,
+  ModalController
+} from 'ionic-angular';
+import {User} from '../../shared/user-model';
+import {AuthServiceProvider} from '../../providers/auth-service/auth-service';
+import {GamePage} from '../game/game';
+import {TopicQuizComponent} from '../../components/topic-quiz/topic-quiz';
+import {topic1} from '../../shared/topic1-questions';
+import {topic4} from '../../shared/topic4-questions';
+import {topic3} from '../../shared/topic3-questions';
 
 @Component({selector: 'page-topic-five', templateUrl: 'topic-five.html'})
 export class TopicFivePage {
-
-  @ViewChild('SwipedTabsSlider')SwipedTabsSlider : Slides;
-  @ViewChild('scroll')scroll : Content;
-
-  SwipedTabsIndicator : any = null;
-  tabElementWidth_px : number = 100;
-  tabs : any = [];
-
-  currentUser : User;  
+  // pages: Array<{title: string, component: any, collection: any}>;
+  currentUser: User;
   events: any[];
+
+  constructor(public navCtrl : NavController, public navParams : NavParams, public modalCtrl : ModalController, private auth : AuthServiceProvider) {
+  }
   
-  constructor(public navCtrl : NavController, private auth : AuthServiceProvider) {
-    this.tabs = ["Problem", "Cause", "Effect", "Importance", "Quiz"];
+  ionViewDidLoad() {}
+
+  gotoQuizOne() {
+    const modal = this
+      .modalCtrl
+      .create(TopicQuizComponent, {"collection": topic1});
+    modal.present();
+  }
+
+  gotoQuizThree() {
+    const modal = this
+      .modalCtrl
+      .create(TopicQuizComponent, {"collection": topic3});
+    modal.present();
+  }
+
+  gotoQuizFour() {
+    const modal = this
+      .modalCtrl
+      .create(TopicQuizComponent, {"collection": topic4});
+    modal.present();
+  }
+
+  gotoGame() {
+    this
+    .navCtrl
+    .push(GamePage);
   }
 
   getPageData() {
@@ -32,13 +65,13 @@ export class TopicFivePage {
             .toJSON();
           y["$key"] = element.key;
 
-          if(y["email"]==this.auth.currentUser.email){
+          if (y["email"] == this.auth.currentUser.email) {
             this.currentUser = y as User;
           }
         });
       });
 
-      this
+    this
       .auth
       .getDBEvents()
       .snapshotChanges()
@@ -61,34 +94,4 @@ export class TopicFivePage {
       });
   }
 
-
-  ionViewDidEnter() {
-    this.SwipedTabsIndicator = document.getElementById("indicator");
-  }
-
-  selectTab(index) {
-    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (100 * index) + '%,0,0)';
-    this
-      .scroll
-      .scrollTo(index * this.tabElementWidth_px, 0, 500);
-    this
-      .SwipedTabsSlider
-      .slideTo(index, 500);
-  }
-
-  updateIndicatorPosition() {
-    this
-      .scroll
-      .scrollTo(this.SwipedTabsSlider.getActiveIndex() * this.tabElementWidth_px, 0, 200);
-
-    // this condition is to avoid passing to incorrect index
-    if (this.SwipedTabsSlider.length() > this.SwipedTabsSlider.getActiveIndex()) {
-      this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (this.SwipedTabsSlider.getActiveIndex() * 100) + '%,0,0)';
-    }
-  }
-
-  animateIndicator($event) {
-    if (this.SwipedTabsIndicator) 
-      this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (($event.progress * (this.SwipedTabsSlider.length() - 1)) * 100) + '%,0,0)';
-    }
-  }
+}
