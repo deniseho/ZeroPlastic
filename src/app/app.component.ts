c2bd4b9691a21fdb78047a1aacb2ba0c97e5f1be
Denise Ho <deniseho630@gmail.com>
2018-08-29 04:54:08 +0100

    modify gotoadchivement link in sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,25 +25,22 @@ import {AlternativesPage} from '../pages/alternatives/alternatives';
 import * as _ from 'lodash';
 import 'jquery';
 import {OnboardingSliderPage} from "../pages/onboarding-slider/onboarding-slider"; // Import jQuery
+import {AchievementPage} from '../pages/achievement/achievement';
 
-@Component({
-  templateUrl: 'app.html'
-})
-export class MyApp{
-  @ViewChild(Nav) nav: Nav;
+@Component({templateUrl: 'app.html'})
+export class MyApp {
+  @ViewChild(Nav)nav : Nav;
   // rootPage = LoginPage;
   rootPage = OnboardingSliderPage;
   users : any;
 
-  pages: Array<{title: string, component: any, avatar: string}>;
+  pages : Array < {
+    title: string,
+    component: any,
+    avatar: string
+  } >;
 
-  constructor(
-    public platform: Platform,
-    public menu: MenuController,
-    public statusBar: StatusBar,
-    public splashScreen: SplashScreen,
-    private auth: AuthServiceProvider,
-  ) {
+  constructor(public platform : Platform, public menu : MenuController, public statusBar : StatusBar, public splashScreen : SplashScreen, private auth : AuthServiceProvider,) {
     this.initializeApp();
     console.log("this.auth.getAllUsers()");
     console.log(this.auth.getAllUsers());
@@ -79,50 +76,33 @@ export class MyApp{
         statusBar.styleDefault();
         splashScreen.hide();
 
-        // platform.registerBackButtonAction(() => {
-
-        //   let nav = app.getActiveNavs()[0];
-        //   let activeView = nav.getActive();
-
-        //   if (activeView.name === "FirstPage") {
-
-        //     if (nav.canGoBack()) { //Can we go back?
-        //       nav.pop();
-        //     } else {
-        //       const alert = this
-        //         .alertCtrl
-        //         .create({
-        //           title: 'App termination',
-        //           message: 'Do you want to close the app?',
-        //           buttons: [
-        //             {
-        //               text: 'Cancel',
-        //               role: 'cancel',
-        //               handler: () => {
-        //                 console.log('Application exit prevented!');
-        //               }
-        //             }, {
-        //               text: 'Close App',
-        //               handler: () => {
-        //                 this
-        //                   .platform
-        //                   .exitApp(); // Close this application
-        //               }
-        //             }
-        //           ]
-        //         });
-        //       alert.present();
-        //     }
-        //   }
-        // });
+        // platform.registerBackButtonAction(() => {   let nav = app.getActiveNavs()[0];
+        //   let activeView = nav.getActive();   if (activeView.name === "FirstPage") {
+        //    if (nav.canGoBack()) { //Can we go back?       nav.pop();     } else {
+        //   const alert = this         .alertCtrl         .create({           title:
+        // 'App termination',           message: 'Do you want to close the app?',
+        //    buttons: [             {               text: 'Cancel',               role:
+        // 'cancel',               handler: () => {
+        // console.log('Application exit prevented!');               }             }, {
+        //              text: 'Close App',               handler: () => {
+        //  this                   .platform                   .exitApp(); // Close this
+        // application               }             }           ]         });
+        // alert.present();     }   } });
       });
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+    this
+      .platform
+      .ready()
+      .then(() => {
+        this
+          .statusBar
+          .styleDefault();
+        this
+          .splashScreen
+          .hide();
+      });
   }
 
   gotoPage(page) {
@@ -143,16 +123,26 @@ export class MyApp{
       .setRoot(TopicMenu);
   }
 
-  logout(){
-    this.auth.logout().subscribe(succ => {
-      this.menu.close();
-      this.nav.setRoot(LoginPage);
-    });
+  logout() {
+    this
+      .auth
+      .logout()
+      .subscribe(succ => {
+        this
+          .menu
+          .close();
+        this
+          .nav
+          .setRoot(LoginPage);
+      });
   }
 
-    /*gotoAchievement() {
-        this
-            .navCtrl
-            .push(AchievementPage);
-    }*/
+  gotoAchievement() {
+    this
+      .menu
+      .close();
+    this
+      .nav
+      .setRoot(AchievementPage);
+  }
 }
